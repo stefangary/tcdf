d320c7d1bc8c09eb82317ab048f386f33f35735c
stefangary <stefanfgary@gmail.com>
2018-09-25 16:11:16 +0100

    Added merge_hist draft and stub file

---
+++ src/tcdf_stub.f90
@@ -0,0 +1,58 @@
+!--------------------------------------
+! Placeholder empty file for starting
+! new routines.
+!
+! As is, just lists back everything on
+! the command line.
+!
+! Stefan Gary, 2018
+!--------------------------------------
+! This software is distributed under the
+! terms of the GNU GPL v3 or any later
+! version.
+!--------------------------------------
+program tcdf_hist_merge
+
+  use basicfun
+  use netcdfio
+  
+  implicit none
+
+  ! Command line args, File names.
+  integer :: num_command_arg
+  integer :: arg_len, arg_count
+  character(len=50) :: arg_string
+  character(len=10) :: arg_int
+  character(len=5) :: arg_flag
+  character(len=10) :: arg_real
+
+  ! Read the command line and figure out
+  ! how many files are listed.
+  num_command_arg = command_argument_count()
+       
+  if(num_command_arg .lt. 1) then
+     write(*,*) ' Error: Must specify at least 1 input file!'
+     call print_help()
+     stop
+  else
+     arg_count = 1
+
+     ! Loop through all the command line files.
+     do
+        ! Test that we are still reading all the files.
+        if ( arg_count .gt. num_command_arg ) exit
+
+        call get_command_argument(arg_count, arg_string, arg_len, exitcode)
+        call checkexit(exitcode)
+        arg_count = arg_count + 1
+
+        write(*,*) arg_string
+     enddo
+  endif
+end program tcdf_hist_merge
+
+subroutine print_help()
+
+  write(*,*) 'Placeholder for printing help.'
+  
+end subroutine print_help
