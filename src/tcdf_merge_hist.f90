ea116fbf12a75fb84839c1821abedb1a087c6c16
stefangary <stefanfgary@gmail.com>
2018-11-09 22:15:53 -0500

    tcdf_merge_hist command line tune up

--- src/tcdf_merge_hist.f90
+++ src/tcdf_merge_hist.f90
@@ -21,33 +21,107 @@ program tcdf_hist_merge
   character(len=5) :: arg_flag
   character(len=10) :: arg_real
 
+  ! Default output file name
+  character(len=50) :: merged_hist_file_name
+  merged_hist_file_name = 'merged_hist.nc'
+  
   ! Read the command line and figure out
   ! how many files are listed.
   num_command_arg = command_argument_count()
        
   if(num_command_arg .lt. 1) then
-     write(*,*) ' Error: Must specify at least 1 input file!'
+     write(*,*) ' Error: Must specify at least 1 flag!'
      call print_help()
      stop
   else
      arg_count = 1
 
-     ! Loop through all the command line files.
+     ! Loop through all the command line flags.
      do
-        ! Test that we are still reading all the files.
+        ! Test that we are still reading all the flags.
         if ( arg_count .gt. num_command_arg ) exit
-
-        call get_command_argument(arg_count, arg_string, arg_len, exitcode)
+        
+        call get_command_argument(arg_count, arg_flag, arg_len, exitcode)
         call checkexit(exitcode)
         arg_count = arg_count + 1
 
-        write(*,*) arg_string
+        if ( index(trim(arg_flag),'-F') .ne. 0 ) then
+           ! Start to read the file list
+
+           ! Loop through all the input files
+           do
+              ! Test that we are still reading all the files.
+              if ( arg_count .gt. num_command_arg ) exit
+              call get_command_argument(arg_count, arg_string, arg_len, exitcode)
+              call checkexit(exitcode)
+              arg_count = arg_count + 1
+        
+              write(*,*) 'Working on file: ',trim(arg_string),' which is arg_count ',arg_count
+
+              ! For the first file, allocate space for
+              ! the storage variables and also create
+              ! the output file.
+              if ( arg_count .eq. 1 ) then
+                 write(*,*) 'Reading first file to init storage and output...'
+                 ! WORKING HERE
+              else
+                 ! Nothing to be done for all subsequent
+                 ! files.
+              endif
+              
+              ! For all input files, add histogram counts
+              ! into the storage histograms.
+              
+           enddo
+        elseif ( index(trim(arg_flag),'-h') .ne. 0 ) then
+           ! Print help and exit
+           call print_help()
+           stop
+        elseif ( index(trim(arg_flag),'-O') .ne. 0 ) then
+           ! Specify the name of the output file and
+           ! change from default.
+           call get_command_argument(arg_count, arg_string, arg_len, exitcode)
+           call checkexit(exitcode)
+           arg_count = arg_count + 1
+           write(*,*) 'Changing default file name: ',merged_hist_file_name
+           merged_hist_file_name = arg_string
+           write(*,*) 'to: ',merged_hist_file_name
+           
+        else
+           write(*,*) 'ERROR: tcdf_hist_merge unrecognized flag'
+        endif
      enddo
   endif
 end program tcdf_hist_merge
 
 subroutine print_help()
 
-  write(*,*) 'Placeholder for printing help.'
+  write(*,*) '---------------------------------'
+  write(*,*) 'tcdf_merge_hist'
+  write(*,*) '---------------------------------'
+  write(*,*) 'usage:'
+  write(*,*) 'tcdf_merge_hist [-O] [-h] -F file1.nc file2.nc ...'
+  write(*,*) '---------------------------------'
+  write(*,*) 'Histogram variables within each'
+  write(*,*) 'file in this list will be added up'
+  write(*,*) 'into a single histogram, a new'
+  write(*,*) 'output file [merged_hist.nc] or'
+  write(*,*) 'name specified by -O. -h prints'
+  write(*,*) 'this help message. -F simply tells'
+  write(*,*) 'the command line interpreter that'
+  write(*,*) 'a list of files is coming and there'
+  write(*,*) 'are no more flags/options after'
+  write(*,*) 'this list.'
+  write(*,*) ' '
+  write(*,*) 'This program assumes all histograms'
+  write(*,*) 'in all input files will have exactly'
+  write(*,*) 'the same dimensions and variable names.'
+  write(*,*) 'If not, then it simply crashes.'
+  write(*,*) 'This was written to work with default'
+  write(*,*) 'output from tcdfhist.'
+  write(*,*) ' '
+  write(*,*) 'Stefan Gary, 2018, distributed under'
+  write(*,*) 'the terms of the GNU GPL v3 or later.'
+  write(*,*) '----------------------------------'
   
 end subroutine print_help
